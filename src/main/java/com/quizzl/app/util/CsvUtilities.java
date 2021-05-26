459af214f13e2c9fbe37fbbd2db53589f016cdc6
Muhamed Mujkic <m.mujkic2001@gmail.com>
2021-05-26 09:01:54 +0200

    Added CSV Import/Export

--- src/main/java/com/quizzl/app/util/CsvUtilities.java
+++ src/main/java/com/quizzl/app/util/CsvUtilities.java
@@ -1,10 +1,12 @@
 package com.quizzl.app.util;
 
+import com.quizzl.app.model.Flashcard;
 import com.quizzl.app.model.FlashcardStaple;
 import com.quizzl.app.repository.FlashcardStapleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.*;
 import java.util.List;
 
 @Component
@@ -19,15 +21,34 @@ public class CsvUtilities {
 
     }
 
-    public static void exportCards(FlashcardStaple staple, String path)
-    {
-        // export to csv
+    public static void exportCards(FlashcardStaple staple, String path) throws IOException {
+        List<Flashcard> flashcardList = staple.getFlashcardList();
+        int listIteratorCounter = 0;
 
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+            while (flashcardList.size() > listIteratorCounter) {
+                bw.write(flashcardList.get(listIteratorCounter).getQuestion());
+                bw.write(";");
+                bw.write(flashcardList.get(listIteratorCounter).getAnswer());
+                bw.newLine();
+                listIteratorCounter++;
+            }
+        }
     }
 
 
-    public static void importCards(String path, boolean isFlashcard)
-    {
-      
+    public static FlashcardStaple importCards(String path) throws IOException {
+        FlashcardStaple importedFlashcardStaple = new FlashcardStaple();
+        String[] data;
+        String line;
+        try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+            while ((line = br.readLine()) != null) {
+                data = line.split(";");
+                for(String part : data) {
+                    importedFlashcardStaple.getFlashcardList.add(part);
+                }
+            }
+        }
+        return importedFlashcardStaple;
     }
 }
